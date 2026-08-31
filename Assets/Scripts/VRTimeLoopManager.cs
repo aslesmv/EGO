8f35e51952d4cb5430150da43254412ba72247b0
aslesmv <aslesmv@gmail.com>
2026-08-31 12:04:30 +1200

    31/08 - 12:04pm
    Starting to implement the time loop system into a vr space

---
+++ Assets/Scripts/VRTimeLoopManager.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRTimeLoopManager : MonoBehaviour
+{
+    [System.Serializable]
+    public struct VRFrameState
+    {
+        public float timeStamp;
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
